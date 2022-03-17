5d7f5e028def960e4a05e78a8649d7638b9a7d21
nospame <mapson.evan@gmail.com>
2022-03-17 16:14:57 -0700

    refactor to use each.with_index

--- euler-22.rb
+++ euler-22.rb
@@ -2,21 +2,15 @@ require 'csv'
 
 # generate the key to assign each letter a value
 key = {}
-i = 1
-('A'..'Z').each do |letter|
-  key[letter] = i
-  i += 1
-end
+('A'..'Z').each.with_index(1) {|letter, i| key[letter] = i}
 
 # read in the names file as a csv
-names = CSV.read("p022_names.txt")[0]
+names = CSV.read("p022_names.txt")[0].sort
 
 # sort the names, get the value for each, and total them up
-i = 1
 total = 0
-names.sort.each do |name|
+names.each.with_index(1) do |name, i|
   total += name.chars.map{|letter| key[letter]}.sum * i
-  i += 1
 end
 
 p total
