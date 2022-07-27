9567db61ffde5baf959ad77d8f0c3d753fd052f1
nospame <mapson.evan@gmail.com>
2022-07-27 10:56:21 -0700

    add 2048 swipe_right method

--- 2048.rb
+++ 2048.rb
@@ -1,20 +1,42 @@
 def swipe_left(row)
-  new_row = row.select { |num| num != 0 }
+  row.select! { |num| num != 0 }
 
-  new_row.each_with_index do |num, i|
-    if new_row[i + 1] == num
-      new_row[i] = num * 2
-      new_row.delete_at(i + 1)
+  row.each_with_index do |num, i|
+    if row[i + 1] == num
+      row[i] = num * 2
+      row.delete_at(i + 1)
     end
   end
 
-  until new_row.length == 4
-    new_row.push(0)
+  until row.length == 4
+    row.push(0)
   end
 
-  new_row
+  row
 end
 
-p swipe_left([2, 0, 4, 8])
-p swipe_left([0, 4, 4, 8])
-p swipe_left([2, 2, 4, 4])
+p swipe_left([2, 0, 4, 8]) # => [2, 4, 8, 0]
+p swipe_left([0, 4, 4, 8]) # => [8, 8, 0, 0]
+p swipe_left([2, 2, 4, 4]) # => [4, 8, 0, 0]
+
+def swipe_right(row)
+  row.reverse!
+  row.select! { |num| num != 0 }
+
+  row.each_with_index do |num, i|
+    if row[i + 1] == num
+      row[i] = num * 2
+      row.delete_at(i + 1)
+    end
+  end
+
+  until row.length == 4
+    row.push(0)
+  end
+
+  row.reverse
+end
+
+p swipe_right([2, 0, 4, 8]) # => [0, 2, 4, 8]
+p swipe_right([0, 4, 4, 8]) # => [0, 0, 8, 8]
+p swipe_right([2, 2, 4, 4]) # => [0, 0, 4, 8]
