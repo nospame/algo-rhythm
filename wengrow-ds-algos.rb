3666ef5082ce4835207bc974ca2eebebdfd252f2
nospame <mapson.evan@gmail.com>
2022-07-26 19:01:36 -0700

    add chapter 10 ds/algo recursion exercises

--- wengrow-ds-algos.rb
+++ wengrow-ds-algos.rb
@@ -80,3 +80,48 @@ def reverse_string(str)
 end
 
 p reverse_string("abcde") # => "edcba"
+
+
+# Chapter 10
+# 3.
+def sum(low, high)
+  if high == low
+    return low
+  end
+  return high + sum(low, high - 1)
+end
+
+p sum(1, 10)
+
+#4.
+def print_numbers(arr)
+  for item in arr do
+    if item.class == Array
+      print_numbers(item)
+    else
+      p item
+    end
+  end
+end
+
+array = [ 
+  1, 
+  2, 
+  3, 
+  [4, 5, 6], 
+  7, 
+  [8, 
+    [9, 10, 11, 
+      [12, 13, 14]
+      ]
+    ], 
+    [15, 16, 17, 18, 19, 
+        [20, 21, 22, 
+          [23, 24, 25, 
+            [26, 27, 29] 
+          ], 30, 31
+        ], 32
+      ], 33
+    ]
+
+print_numbers(array)
